9db9592310c1a916be61620a4d11fada8097ffce
X Zhang <zhangxd1@gmail.com>
2013-07-05 05:23:27 +0000

    update web.js

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var content = fs.readFileSync('index.html');
+   console.log(content.toString());
+  response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
